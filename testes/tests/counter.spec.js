0b7340a9659802da608a3bd8fe1afe4da7fdc1bd
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-04-03 17:25:07 -0300

    feat(testes): módulo sobre test com mohca

---
+++ testes/tests/counter.spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('testando contador', function() {
+  it('contador deve ser iniciar com valor zero', function() {
+    const counterResult = 0;
+    assert.equal(counterResult, 1);
+  });
+});
